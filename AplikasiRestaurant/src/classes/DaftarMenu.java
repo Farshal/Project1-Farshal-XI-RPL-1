330e6ee87e7f306929f976244dab699c10411045
Farshal <fyuzka@gmail.com>
2021-06-01 15:14:36 +0700

    Implementasi Kelas Ramen, Kuah, Toping dan Minuman

--- AplikasiRestaurant/src/classes/DaftarMenu.java
+++ AplikasiRestaurant/src/classes/DaftarMenu.java
@@ -4,11 +4,20 @@
  * and open the template in the editor.
  */
 package classes;
-
+import java.util.ArrayList;
 /**
  *
  * @author Farshal-Thinkpad
  */
 public class DaftarMenu {
+    private ArrayList<Menu> daftarmenu;
+    
+    public DaftarMenu() {}
+    
+    public void tambahMenu(Menu menu) {}
+    
+    public void getmenuByKategori(String kategori) {}
+    
+    public void tampilDaftarMenu() {}
     
 }
